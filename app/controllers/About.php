f57c983420d81e9ae59414c35dd1d93db4299c73
farhanzayid1982 <farhan.zayid@gmail.com>
2025-07-03 18:40:10 +0700

    modified:   app/controllers/About.php

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -3,8 +3,6 @@
     class About extends Controller{
         public function index($param1 = 'Nilai default', $param2 = 'Nilai default', $param3 = 'Nilai default')
         {
-            //echo 'About/index';
-            //echo "ini adalah paramter pertama $param1 dan ini adalah parameter ke dua $param2";
             $data['param1'] = $param1;
             $data['param2'] = $param2;
             $data['param3'] = $param3;
@@ -16,7 +14,6 @@
 
         public function page()
         {
-            //echo 'About/page';
             $data['judul_halaman'] = 'About Page';
             $this->view('templates/header', $data);
             $this->view('about/page');
