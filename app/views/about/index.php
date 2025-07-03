373a41fae9907dfaedaa2e6325ceb9b175274ad3
farhanzayid1982 <farhan.zayid@gmail.com>
2025-07-03 19:04:36 +0700

    modified:   app/views/about/index.php 	modified:   app/views/about/page.php

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,5 +1,5 @@
 <div class="container">
-    <h1 class="mt-4">About Index</h1>
+    <h1 class="mt-4"><?= $data['judul_halaman'] ?></h1>
     <p>Berikut ini adalah paramter yang dikirimkan</p>
     <p>Ini parameter pertama <?= $data['param1'] ?></p>
     <p>Ini parameter kedua <?= $data['param2'] ?></p>
--- app/views/about/page.php
+++ app/views/about/page.php
@@ -1,3 +1,4 @@
 <div class="container mt-4">
     <h1>About Page</h1>
+    <?= $data['judul_halaman'] ?>
 </div>
